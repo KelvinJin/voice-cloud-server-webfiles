7e0c21d19b01b05d6375cc128c7760cd6b7d0880
owenwjowenwj@gmail.com <owenwjowenwj@gmail.com@3b111edb-037a-4f9f-9c0c-959316dcd08f>
2012-03-11 14:50:35 +0000

    upgrade with database!

--- trunk/uploadify/check.php
+++ trunk/uploadify/check.php
@@ -26,7 +26,7 @@ THE SOFTWARE.
 $fileArray = array();
 foreach ($_POST as $key => $value) {
 	if ($key != 'folder') {
-		if (file_exists($_SERVER['DOCUMENT_ROOT'] . $_POST['folder'] . '/' . $value)) {
+		if (file_exists($_POST['folder'] . '/' . $value)) {
 			$fileArray[$key] = $value;
 		}
 	}
