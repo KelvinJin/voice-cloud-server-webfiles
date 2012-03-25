0d72e3446f64326225981d68fd020f4fc47b9e12
wy2007121@gmail.com <wy2007121@gmail.com@3b111edb-037a-4f9f-9c0c-959316dcd08f>
2012-03-25 14:02:42 +0000

    New web pattern design.

---
+++ client_download.php
@@ -0,0 +1,28 @@
+<?php 
+	$file_dir = '/home/owenwj/prog/';
+	$name = 'Client.zip';
+	if (!file_exists($file_dir.$name)){
+	
+		header("Content-type: text/html; charset=utf-8");
+	
+		echo "File not found!";
+	
+		exit;
+	
+	} else {
+	
+		$file = fopen($file_dir.$name,"r");
+	
+		Header("Content-type: application/octet-stream");
+	
+		Header("Accept-Ranges: bytes");
+	
+		Header("Accept-Length: ".filesize($file_dir . $name));
+	
+		Header("Content-Disposition: attachment; filename=".$name);
+	
+		echo fread($file, filesize($file_dir.$name));
+		fclose($file);
+	}
+?>
+
